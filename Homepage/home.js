1b5ca03989b6f412b5bb750b769227774b63bb11
nathan-dza <160131368+nathan-dza@users.noreply.github.com>
2026-05-04 20:43:46 -0700

    Added project view pages & finished eecu
    Added in between pages, before you would just have to cycle through all the projects and thats annoying so i just made a page so that you can choose which one to go to

---
+++ Homepage/home.js
@@ -0,0 +1,15 @@
+const resume = document.getElementById('resumeRedirect');
+const projects = document.getElementById('portfilioRerirect');
+const abMe = document.getElementById('aboutMeRedirect');
+
+resume.addEventListener('click', function(){
+    window.location.href = '../Resume/resume.html';
+});
+
+projects.addEventListener('click', function(){
+    window.location.href = '../Projects/projSelect.html';
+});
+
+abMe.addEventListener('click', function(){
+    window.location.href = '../Writing-Projects/wProjSelect.html';
+});
