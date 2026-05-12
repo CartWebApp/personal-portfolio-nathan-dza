4031540a243c2b526d3f4dd56b700b52f470b645
nathan-dza <160131368+nathan-dza@users.noreply.github.com>
2026-05-11 23:34:14 -0700

    Finishing touches

--- Homepage/home.js
+++ Homepage/home.js
@@ -2,6 +2,8 @@ const resume = document.getElementById('resumeRedirect');
 const projects = document.getElementById('portfilioRerirect');
 const abMe = document.getElementById('aboutMeRedirect');
 
+
+// redirects
 resume.addEventListener('click', function(){
     window.location.href = '../Resume/resume.html';
 });
